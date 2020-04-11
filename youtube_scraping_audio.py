c198bbd8eac77fe73f444692eb9a6e2a4dfe3bd2
Shradhit Subudhi <shradhit@gmail.com>
2020-04-11 01:13:18 -0400

    Update youtube_scraping_audio.py

--- youtube_scraping_audio.py
+++ youtube_scraping_audio.py
@@ -27,5 +27,19 @@ ydl_opts = {
 
 
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([input_])
+    meta = ydl.extract_info(input_, download=True) 
     
+    
+print('upload date : %s' %(meta['upload_date']))
+print('uploader    : %s' %(meta['uploader']))
+print('views       : %d' %(meta['view_count']))
+print('likes       : %d' %(meta['like_count']))
+print('dislikes    : %d' %(meta['dislike_count']))
+print('id          : %s' %(meta['id']))
+print('format      : %s' %(meta['format']))
+print('duration    : %s' %(meta['duration']))
+print('title       : %s' %(meta['title']))
+print('description : %s' %(meta['description']))
+
+
+
